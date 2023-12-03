42993eb7d82218e4e80cd3cf73b81d95af808252
Leos <136538188+MuraMach@users.noreply.github.com>
2023-12-03 16:59:25 -0330

    Update Null.py
    D-PortV0.00.02->0.00.03N.01

--- NeoxaSharedMN/TSMN[Scripts]/Null.py
+++ NeoxaSharedMN/TSMN[Scripts]/Null.py
@@ -1,1 +1,92 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
+# Replace 'rpcuser', 'rpcpassword', and 'rpcport' with your NeoxaCore RPC credentials
+rpc_connection = AuthServiceProxy("http://rpcuser:rpcpassword@localhost:rpcport")
+
+def neoxa_rpc_call(command, *params):
+    try:
+        return rpc_connection.__getattr__(command)(*params)
+    except JSONRPCException as e:
+        print(f"Error executing RPC command '{command}': {e}")
+        return None
+
+def multisig_masternode_script(user_signature, whitelisted_users, current_block_height, lock_until_block_height,
+                               masternode_proposal_flag, user_masternode_proposal, proposal_id, creator_address,
+                               proposal_comments, proposal_metadata, proposal_expiry_blocks,
+                               proposal_rejection_threshold, voting_start_block_height, masternode_operators,
+                               operator_opt_out_flag, operator_redemption_signature, redemption_block_height,
+                               operator_signature):
+    script = []
+
+    # Check if the provided signature matches a whitelisted participant
+    script.extend([user_signature] + whitelisted_users + [f"{len(whitelisted_users)} OP_CHECKMULTISIGVERIFY"])
+
+    # Check if the collateral deposit is locked for masternode
+    script.extend([f"{current_block_height} {lock_until_block_height} OP_LE OP_VERIFY"])
+
+    # Masternode proposal submission logic
+    script.extend([masternode_proposal_flag, "OP_IF"])
+    # User's masternode proposal
+    script.extend([user_masternode_proposal, f"{user_signature} OP_CHECKSIGVERIFY"])
+    # Masternode proposal data
+    script.extend([f"\"Masternode Proposal Title\" \"Masternode Proposal Description\" 1000 {proposal_id} {creator_address} " 
+                   f"\"{proposal_comments}\" \"Pending\" \"{proposal_metadata}\" {proposal_expiry_blocks} "
+                   f"{proposal_rejection_threshold}"])
+    # Proposal locking once voting starts
+    script.extend([f"{voting_start_block_height} {current_block_height} OP_LE OP_VERIFY"])
+    # Additional custom voting logic for masternode proposals
+    script.extend(masternode_operators)
+    # Count the number of masternode operator votes
+    script.extend(["0 OP_TOALTSTACK"])
+    script.extend([f"{len(masternode_operators)} OP_ADD"])
+    # Dynamic voting threshold calculation
+    script.extend(["OP_2 OP_DIV OP_TOALTSTACK OP_DUP OP_NUMEQUALVERIFY OP_FROMALTSTACK OP_NUMEQUALVERIFY"])
+    # Proposal expiry
+    script.extend([f"{current_block_height} {proposal_expiry_blocks} OP_SUB OP_GREATERTHAN OP_NOTIF"])
+    script.extend(["\"Expired\" OP_RETURN OP_ENDIF"])
+    # Masternode operators can opt-out or be removed
+    script.extend([operator_opt_out_flag, "OP_IF"])
+    script.extend(["\"Rejected\" OP_RETURN OP_ENDIF"])
+    # Proposal voting timeframe
+    script.extend([f"{current_block_height} {voting_start_block_height} OP_SUB 1440 OP_GREATERTHAN OP_NOTIF"])
+    script.extend(["\"Rejected\" OP_RETURN OP_ENDIF"])
+    # Redemption logic for masternode operators
+    script.extend([operator_redemption_signature, f"{current_block_height} {redemption_block_height} OP_LE OP_VERIFY"])
+    script.extend([f"{operator_signature} OP_CHECKSIGVERIFY", "\"Redemption\" OP_RETURN"])
+    script.extend(["OP_ENDIF"])
+    return " ".join(script)
+
+# Example Usage
+user_signature = "<user_signature>"
+whitelisted_users = ["<whitelisted_user1_public_key>", "<whitelisted_user2_public_key>",
+                     "<whitelisted_user3_public_key>", "<whitelisted_user4_public_key>",
+                     "<whitelisted_user5_public_key>"]
+current_block_height = 1000
+lock_until_block_height = 1100
+masternode_proposal_flag = "<masternode_proposal_flag>"
+user_masternode_proposal = "<user_masternode_proposal>"
+proposal_id = 123
+creator_address = "<creator_address>"
+proposal_comments = "Sample Proposal"
+proposal_metadata = "Sample Metadata"
+proposal_expiry_blocks = 1200
+proposal_rejection_threshold = 2
+voting_start_block_height = 1050
+masternode_operators = ["<masternode_operator1_public_key>", "<masternode_operator2_public_key>",
+                        "<masternode_operator3_public_key>", "<masternode_operator4_public_key>",
+                        "<masternode_operator5_public_key>"]
+operator_opt_out_flag = "<operator_opt_out_flag>"
+operator_redemption_signature = "<operator_redemption_signature>"
+redemption_block_height = 1090
+operator_signature = "<operator_signature>"
+
+multisig_script = multisig_masternode_script(user_signature, whitelisted_users, current_block_height,
+                                              lock_until_block_height, masternode_proposal_flag,
+                                              user_masternode_proposal, proposal_id, creator_address,
+                                              proposal_comments, proposal_metadata, proposal_expiry_blocks,
+                                              proposal_rejection_threshold, voting_start_block_height,
+                                              masternode_operators, operator_opt_out_flag,
+                                              operator_redemption_signature, redemption_block_height,
+                                              operator_signature)
+
+print(f"Generated MultiSig Script: {multisig_script}")
